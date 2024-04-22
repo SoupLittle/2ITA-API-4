1a214a55f3fe9dacd122011abde6767ad084be9e
SoupLittle <marleneturlid@gmail.com>
2024-04-22 10:00:36 +0200

    *

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+
 window.onload = function() {
     fetch('https://randomuser.me/api')
     .then(response => response.json())
     .then(data => {
-        const user = data.results[0];
+        var user = data.results[0];
         document.getElementById('avatarImg').src = user.picture.large;
         document.getElementById('name').textContent = user.name.first + ' ' + user.name.last;
         document.getElementById('address').textContent = user.location.street.number + ' ' + user.location.street.name + ', ' + user.location.city + ', ' + user.location.country;
